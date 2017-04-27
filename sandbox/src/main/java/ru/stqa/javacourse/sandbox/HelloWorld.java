a64a4f1a8966de973447a3ed0b9f2d7664c19557
annakostomarova <anna.iv.kostomarova@gmail.com>
2017-04-27 14:26:56 +0300

    Изменено форматирование

--- sandbox/src/main/java/ru/stqa/javacourse/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/javacourse/sandbox/HelloWorld.java
@@ -1,9 +1,9 @@
 package ru.stqa.javacourse.sandbox;
 
-class HelloWorld {
+public class HelloWorld {
 
 	public static void main(String[] args) {
-
 		System.out.println("Hello world!");
 	}
-	}
+
+}
