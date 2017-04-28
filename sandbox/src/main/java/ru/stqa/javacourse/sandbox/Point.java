9707a474ddecaae27dbd77a0aed6c2bc21e5a8aa
annakostomarova <anna.iv.kostomarova@gmail.com>
2017-04-28 23:08:45 +0300

    Добавлены классы Point и PointRun

---
+++ sandbox/src/main/java/ru/stqa/javacourse/sandbox/Point.java
@@ -0,0 +1,16 @@
+package ru.stqa.javacourse.sandbox;
+
+/**
+ * Created by kostoa on 4/27/2017.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+}
---
+++ sandbox/src/main/java/ru/stqa/javacourse/sandbox/PointRun.java
@@ -0,0 +1,19 @@
+package ru.stqa.javacourse.sandbox;
+
+/**
+ * Created by kostoa on 4/28/2017.
+ */
+public class PointRun {
+
+  public static void main(String[] args) {
+
+    Point p1 = new Point(7.0, 2.0);
+    Point p2 = new Point(1.0, -5.0);
+
+    System.out.println("Расстояние между двумя точками равно " + " = " + distance(p1,p2));
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+  }
+}
