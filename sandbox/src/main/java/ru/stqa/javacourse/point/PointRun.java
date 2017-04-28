4baa32ab42857117c74609798daf7777cc8c33a2
annakostomarova <anna.iv.kostomarova@gmail.com>
2017-04-29 01:24:06 +0300

    В классах Point и PointRun поправлены недочеты

--- sandbox/src/main/java/ru/stqa/javacourse/point/PointRun.java
+++ sandbox/src/main/java/ru/stqa/javacourse/point/PointRun.java
@@ -1,6 +1,6 @@
 package ru.stqa.javacourse.point;
 
-import ru.stqa.javacourse.point.Point;
+import static ru.stqa.javacourse.point.Point.distance;
 
 /**
  * Created by kostoa on 4/28/2017.
@@ -15,7 +15,5 @@ public class PointRun {
     System.out.println("Расстояние между двумя точками равно " + " = " + distance(p1,p2));
   }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-  }
+
 }
