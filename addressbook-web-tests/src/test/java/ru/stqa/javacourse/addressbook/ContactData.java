34cf99de57d59bd66be54277705d6b7c8acb4208
annakostomarova <anna.iv.kostomarova@gmail.com>
2017-04-29 16:58:04 +0300

    Создан тест для создания контактов
    Созданы классы ContactData и ContactCreationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.javacourse.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String nickname;
+  private final String address;
+  private final String homephone;
+  private final String mobilephone;
+
+  public ContactData(String firstname, String lastname, String nickname, String address, String homephone, String mobilephone) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.address = address;
+    this.homephone = homephone;
+    this.mobilephone = mobilephone;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomephone() {
+    return homephone;
+  }
+
+  public String getMobilephone() {
+    return mobilephone;
+  }
+}
