52ef136adb4183a2a0cc8f9b94108e70499cd096
annakostomarova <anna.iv.kostomarova@gmail.com>
2017-05-22 19:09:17 +0300

    Добавлены тесты проверки путём сравнения списков. Списки упорядочены

--- addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/model/ContactData.java
@@ -41,7 +41,7 @@ public class ContactData {
                      String phone2,
                      String notes,
                      String group) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.middlename = middlename;
     this.lastname = lastname;
@@ -214,51 +214,16 @@ public class ContactData {
 
     ContactData that = (ContactData) o;
 
-    if (id != that.id) return false;
     if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
-    if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
-    if (middlename != null ? !middlename.equals(that.middlename) : that.middlename != null) return false;
-    if (nickname != null ? !nickname.equals(that.nickname) : that.nickname != null) return false;
-    if (company != null ? !company.equals(that.company) : that.company != null) return false;
-    if (title != null ? !title.equals(that.title) : that.title != null) return false;
-    if (address != null ? !address.equals(that.address) : that.address != null) return false;
-    if (homephone != null ? !homephone.equals(that.homephone) : that.homephone != null) return false;
-    if (mobilephone != null ? !mobilephone.equals(that.mobilephone) : that.mobilephone != null) return false;
-    if (workphone != null ? !workphone.equals(that.workphone) : that.workphone != null) return false;
-    if (fax != null ? !fax.equals(that.fax) : that.fax != null) return false;
-    if (email != null ? !email.equals(that.email) : that.email != null) return false;
-    if (email2 != null ? !email2.equals(that.email2) : that.email2 != null) return false;
-    if (email3 != null ? !email3.equals(that.email3) : that.email3 != null) return false;
-    if (homepage != null ? !homepage.equals(that.homepage) : that.homepage != null) return false;
-    if (address2 != null ? !address2.equals(that.address2) : that.address2 != null) return false;
-    if (phone2 != null ? !phone2.equals(that.phone2) : that.phone2 != null) return false;
-    if (notes != null ? !notes.equals(that.notes) : that.notes != null) return false;
-    return group != null ? group.equals(that.group) : that.group == null;
+    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
 
   }
 
   @Override
   public int hashCode() {
-    int result = id;
-    result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+    int result = firstname != null ? firstname.hashCode() : 0;
     result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-    result = 31 * result + (middlename != null ? middlename.hashCode() : 0);
-    result = 31 * result + (nickname != null ? nickname.hashCode() : 0);
-    result = 31 * result + (company != null ? company.hashCode() : 0);
-    result = 31 * result + (title != null ? title.hashCode() : 0);
-    result = 31 * result + (address != null ? address.hashCode() : 0);
-    result = 31 * result + (homephone != null ? homephone.hashCode() : 0);
-    result = 31 * result + (mobilephone != null ? mobilephone.hashCode() : 0);
-    result = 31 * result + (workphone != null ? workphone.hashCode() : 0);
-    result = 31 * result + (fax != null ? fax.hashCode() : 0);
-    result = 31 * result + (email != null ? email.hashCode() : 0);
-    result = 31 * result + (email2 != null ? email2.hashCode() : 0);
-    result = 31 * result + (email3 != null ? email3.hashCode() : 0);
-    result = 31 * result + (homepage != null ? homepage.hashCode() : 0);
-    result = 31 * result + (address2 != null ? address2.hashCode() : 0);
-    result = 31 * result + (phone2 != null ? phone2.hashCode() : 0);
-    result = 31 * result + (notes != null ? notes.hashCode() : 0);
-    result = 31 * result + (group != null ? group.hashCode() : 0);
     return result;
   }
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/tests/ContactCreationTests.java
@@ -3,7 +3,9 @@ package ru.stqa.javacourse.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.javacourse.addressbook.model.ContactData;
+import ru.stqa.javacourse.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -38,14 +40,11 @@ public class ContactCreationTests extends TestBase {
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (ContactData c : after) {
-            if (c.getId() > max) {
-                max = c.getId();
-            }
-        }
-        contact.setId(max);
         before.add(contact);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(),c2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
+
     }
 }
