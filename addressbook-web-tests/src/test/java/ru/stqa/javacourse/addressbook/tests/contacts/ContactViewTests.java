c43a7e4054816855456cd7d1f58122da2d999ee6
annakostomarova <anna.iv.kostomarova@gmail.com>
2017-06-08 20:24:26 +0300

    Создан тест для сравнения информации о контакте

--- addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/tests/contacts/ContactViewTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/tests/contacts/ContactViewTests.java
@@ -12,25 +12,24 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactViewTests extends TestBase {
 
-  @Test(enabled = false)
+  @Test
   public void testContactView() {
     app.goTo().homePage();
     ContactData contact = app.contact().all().iterator().next();
 
-
-    ContactData contactInfoFromViewForm = app.contact().infoFromViewForm(contact.getId());
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    ContactData contactInfoFromViewForm = app.contact().infoFromViewForm(contact.getId());
 
     assertThat(app.contact().mergeInfoFromViewForm(contactInfoFromViewForm), equalTo(mergeInfoFromEditForm(contactInfoFromEditForm)));
   }
 
   private String mergeInfoFromEditForm(ContactData contact) {
-    return Arrays.asList(contact.getLastname(),contact.getFirstname(),contact.getMiddlename(),
+    return Arrays.asList(contact.getFirstname(),contact.getLastname(),contact.getAddress(),
             contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(),
-            contact.getEmail(), contact.getEmail2(), contact.getEmail3(),contact.getAddress())
+            contact.getEmail(), contact.getEmail2(), contact.getEmail3())
             .stream().filter((s) -> ! s.equals(""))
             .map(ContactViewTests::cleaned)
-            .collect(Collectors.joining("\n"));
+            .collect(Collectors.joining(""));
   }
 
   public static String cleaned(String allInfo) {
