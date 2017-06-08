61dac9196b09efe93dec7b79680b7c6e44b235b9
annakostomarova <anna.iv.kostomarova@gmail.com>
2017-06-08 11:44:32 +0300

    Изменены поля для сравнения списков контактов

--- addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/model/ContactData.java
@@ -332,27 +332,7 @@ public class ContactData {
     if (id != that.id) return false;
     if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
     if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
-    if (middlename != null ? !middlename.equals(that.middlename) : that.middlename != null) return false;
-    if (nickname != null ? !nickname.equals(that.nickname) : that.nickname != null) return false;
-    if (company != null ? !company.equals(that.company) : that.company != null) return false;
-    if (title != null ? !title.equals(that.title) : that.title != null) return false;
-    if (address != null ? !address.equals(that.address) : that.address != null) return false;
-    if (homephone != null ? !homephone.equals(that.homephone) : that.homephone != null) return false;
-    if (mobilephone != null ? !mobilephone.equals(that.mobilephone) : that.mobilephone != null) return false;
-    if (workphone != null ? !workphone.equals(that.workphone) : that.workphone != null) return false;
-    if (fax != null ? !fax.equals(that.fax) : that.fax != null) return false;
-    if (email != null ? !email.equals(that.email) : that.email != null) return false;
-    if (email2 != null ? !email2.equals(that.email2) : that.email2 != null) return false;
-    if (email3 != null ? !email3.equals(that.email3) : that.email3 != null) return false;
-    if (homepage != null ? !homepage.equals(that.homepage) : that.homepage != null) return false;
-    if (address2 != null ? !address2.equals(that.address2) : that.address2 != null) return false;
-    if (phone2 != null ? !phone2.equals(that.phone2) : that.phone2 != null) return false;
-    if (notes != null ? !notes.equals(that.notes) : that.notes != null) return false;
-    if (group != null ? !group.equals(that.group) : that.group != null) return false;
-    if (allPhones != null ? !allPhones.equals(that.allPhones) : that.allPhones != null) return false;
-    if (allEmails != null ? !allEmails.equals(that.allEmails) : that.allEmails != null) return false;
-    if (viewAllInfo != null ? !viewAllInfo.equals(that.viewAllInfo) : that.viewAllInfo != null) return false;
-    return photo != null ? photo.equals(that.photo) : that.photo == null;
+    return address != null ? address.equals(that.address) : that.address == null;
   }
 
   @Override
@@ -360,27 +340,7 @@ public class ContactData {
     int result = id;
     result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
     result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-    result = 31 * result + (middlename != null ? middlename.hashCode() : 0);
-    result = 31 * result + (nickname != null ? nickname.hashCode() : 0);
-    result = 31 * result + (company != null ? company.hashCode() : 0);
-    result = 31 * result + (title != null ? title.hashCode() : 0);
     result = 31 * result + (address != null ? address.hashCode() : 0);
-    result = 31 * result + (homephone != null ? homephone.hashCode() : 0);
-    result = 31 * result + (mobilephone != null ? mobilephone.hashCode() : 0);
-    result = 31 * result + (workphone != null ? workphone.hashCode() : 0);
-    result = 31 * result + (fax != null ? fax.hashCode() : 0);
-    result = 31 * result + (email != null ? email.hashCode() : 0);
-    result = 31 * result + (email2 != null ? email2.hashCode() : 0);
-    result = 31 * result + (email3 != null ? email3.hashCode() : 0);
-    result = 31 * result + (homepage != null ? homepage.hashCode() : 0);
-    result = 31 * result + (address2 != null ? address2.hashCode() : 0);
-    result = 31 * result + (phone2 != null ? phone2.hashCode() : 0);
-    result = 31 * result + (notes != null ? notes.hashCode() : 0);
-    result = 31 * result + (group != null ? group.hashCode() : 0);
-    result = 31 * result + (allPhones != null ? allPhones.hashCode() : 0);
-    result = 31 * result + (allEmails != null ? allEmails.hashCode() : 0);
-    result = 31 * result + (viewAllInfo != null ? viewAllInfo.hashCode() : 0);
-    result = 31 * result + (photo != null ? photo.hashCode() : 0);
     return result;
   }
 
@@ -390,6 +350,7 @@ public class ContactData {
             "id=" + id +
             ", firstname='" + firstname + '\'' +
             ", lastname='" + lastname + '\'' +
+            ", address='" + address + '\'' +
             '}';
   }
 
